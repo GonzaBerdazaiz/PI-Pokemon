1b10a0388d016d10e5bd3283d6aa810b0d9cc2dc
GonzaBerdazaiz <gonzaloberdazaiz@hotmail.com>
2023-05-11 02:47:46 -0300

    Inicio front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+//import { useLocation } from "react-router-dom";
+import Landing from "./views/Landing/Landing";
+import Home from "./views/Home/Home";
+import style from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={style.App}>
       <h1>Henry Pokemon</h1>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +18,21 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
