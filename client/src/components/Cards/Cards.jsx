3c7d8e3a139ba65fec8cec5d1d49e17be5b03fca
GonzaBerdazaiz <gonzaloberdazaiz@hotmail.com>
2023-05-19 01:13:20 -0300

    19-05

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,48 +1,23 @@
 import Card from "../Card/Card";
 import style from "./Cards.module.css";
-//import {useSelector} from "react-redux"
 
-const Cards = ({ pokemons }) => {
+const Cards = ({ pokemons }) => {   //nos traemos x props los pokemons del currentPage
+
     return (
-      <div className={style.CardsContainer}>
-        {pokemons.map((poke) => {
-          return (
-            <Card
-              key={poke.id}
-              id={poke.id}
-              image={
-                poke.image
-                  ? poke.image
-                  : 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/201.png'
-              }
-              name={poke.name}
-              types={poke.types?.map((v) => v.name).join(' / ')}
-            />
-          );
-        })}
-      </div>
+        <div className={style.CardsContainer}> 
+            {pokemons.map((poke) => {
+            return (
+                <Card
+                key={poke.id}
+                id={poke.id}
+                image={poke.image ? poke.image : 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/201.png'}
+                name={poke.name}
+                types={poke.types?.map((v) => v.name).join(' / ')}
+                />
+            );
+            })}
+        </div>
     );
   };
   
   export default Cards;
-
-// const Cards = (pokemons) => {
-    
-//     const allPokemons = useSelector(state=>state.allPokemons); //nos traemos los pokemones del estado global
-
-//     return(
-//         <div className={style.CardsContainer}>
-//             {allPokemons?.map(poke=>{
-//                 return <Card 
-//                     key={poke.id}
-//                     id={poke.id}
-//                     image = {poke.image ? poke.image : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/201.png"} 
-//                     name = {poke.name}
-//                     types = {poke.types?.map((v) => v.name).join(' / ')}
-//                 />
-//             })}
-//         </div>
-//     )
-// }
-
-// export default Cards;
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -4,13 +4,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import style from "./Home.module.css";
 import { getAllPokemons } from "../../redux/actions";
 import Pagination from "../../components/Paginacion/Paginacion"
+import Loading from "../../components/Loading/Loading";
 
 const Home = () => { //Al montar el componente HOME, debemos ejecutar las actions. Como contiene Cards, Home le pide al Store de redux que haga un cambio
 
     const dispatch = useDispatch();
-  const allPokemons = useSelector((state) => state.allPokemons);
-  const [currentPage, setCurrentPage] = useState(1);
-  const pokemonsPerPage = 12;
+    const allPokemons = useSelector((state) => state.allPokemons);
+    const [currentPage, setCurrentPage] = useState(1);  //los creamos para controlar la pagina actual del paginado
+    const pokemonsPerPage = 12;
+    const [loading, setLoading] = useState(true);   //creamos un estado para manejar la demora
 
     useEffect(()=>{ //para manejar el ciclo de vida del componente. Cuando se monta useEffect recibe una funcion la q se ejecuta cunado el componente se monta o cambia alguna de las dependencias el array de dependencia
         dispatch(getAllPokemons());
@@ -19,47 +21,40 @@ const Home = () => { //Al montar el componente HOME, debemos ejecutar las action
 
     const handlePaginate = (pageNumber) => {
         setCurrentPage(pageNumber);
-      };
+    };
 
-      const indexOfLastPokemon = currentPage * pokemonsPerPage;
-      const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
-      const currentPokemons = allPokemons.slice(
+    const indexOfLastPokemon = currentPage * pokemonsPerPage;
+    const indexOfFirstPokemon = indexOfLastPokemon - pokemonsPerPage;
+    const currentPokemons = allPokemons.slice(
         indexOfFirstPokemon,
         indexOfLastPokemon
-      );
-
-    return (
-        <div className={style.HomeBackground}>
-          <div>
-            <Pagination
-                pokemonsPerPage={pokemonsPerPage}
-                totalPokemons={allPokemons.length}
-                currentPage={currentPage}
-                handlePaginate={handlePaginate}
-            />
-          </div>
-          <Cards pokemons={currentPokemons} />
-        </div>
     );
-  
-};
 
-
-
-    // return(
-    //     <div className={style.HomeBackground}>
-    //         {/* <div>
-    //             <Paginacion
-    //                 setCurrentPage={setCurrentPage}
-    //                 currentPage={currentPage}
-    //                 totalPokemon={allPokemons.length}
-    //                 pokemonPerPage={pokemonPerPage}
-    //             />
-    //         </div> */}
-    //         {/* <Cards Pokemon={currentPokemon}/> */}
-    //         <Cards/>
-    //     </div>
-    // )
-//};
+    const changeLoading = () => {   //si la demora en cargar el home es mayor a lo especificado, metemos el loading
+        setTimeout(() => {
+          setLoading(false);
+        }, 3000);
+    };
+    
+    if (loading) {
+        changeLoading();
+        return <Loading></Loading>;
+    } else {
+        return (
+            <div className={style.HomeBackground}>
+                <div>
+                    <Pagination
+                        pokemonsPerPage={pokemonsPerPage}
+                        totalPokemons={allPokemons.length}
+                        currentPage={currentPage}
+                        handlePaginate={handlePaginate}
+                    />
+                </div>
+                <Cards pokemons={currentPokemons} />    {/*aca pasamos las props al Cards */}
+                
+            </div>
+        );
+    };
+}
 
 export default Home;
